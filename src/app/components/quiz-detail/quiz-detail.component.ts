e7fc6093f273752d2852cbc89568d863509b788e
Chris Rice <cd.rice23@gmail.com>
2024-03-27 14:07:52 -0500

    Added styles and functionality to results page.

--- src/app/components/quiz-detail/quiz-detail.component.ts
+++ src/app/components/quiz-detail/quiz-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { QuizService } from '../../services/quiz.service';
 import { Quiz, QuizQuestion, QuizQuestionType } from '../../models/Quiz';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
@@ -14,7 +14,7 @@ export class QuizDetailComponent implements OnInit {
   quizForm!: FormGroup;
   QuizQuestionType = QuizQuestionType;
 
-  constructor(private route: ActivatedRoute, private quizService: QuizService, private fb: FormBuilder) {}
+  constructor(private route: ActivatedRoute, private quizService: QuizService, private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -38,7 +38,20 @@ export class QuizDetailComponent implements OnInit {
 
 
   onSubmit(): void {
-    // console.log(this.answers);
-    // Handle form submission logic here
+    const userAnswers = this.quizForm.value;
+    let score = 0;
+    const results = this.quiz.questions.map(question => {
+        const userAnswer = userAnswers[question.id.toString()];
+        const isCorrect = typeof question.correctAnswer === "boolean" ? question.correctAnswer === userAnswer : question.correctAnswer?.toLowerCase() === userAnswer.toLowerCase();
+        if (isCorrect) score++;
+        return {
+          question: question.text,
+          userAnswer,
+          correctAnswer: question.correctAnswer,
+          isCorrect
+        };
+    });
+
+    this.router.navigate(['/quiz', this.quiz.id, 'results'], { state: { quiz: this.quiz, results, score } });
   }
 }
--- src/app/components/quiz-results/quiz-results.component.ts
+++ src/app/components/quiz-results/quiz-results.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Quiz } from '../../models/Quiz';
+import { map } from 'rxjs/operators';
+import { faTimes, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-quiz-results',
-  templateUrl: './quiz-results.component.html',
-  styleUrl: './quiz-results.component.scss'
+ selector: 'app-quiz-results',
+ templateUrl: './quiz-results.component.html',
+ styleUrls: ['./quiz-results.component.scss']
 })
-export class QuizResultsComponent {
+export class QuizResultsComponent implements OnInit {
+  quiz!: Quiz;
+  results!: any[];
+  score!: number;
+  faTimes = faTimes;
+  faCheck = faCheck;
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap
+    .pipe(map(() => window.history.state))
+    .subscribe(state => {
+      if (state) {
+        this.quiz = state.quiz;
+        this.results = state.results;
+        this.score = state.score;
+      }
+    });
+  }
 }
